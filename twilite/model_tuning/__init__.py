8a0f23e85d555915df9574fa61dcfc71fe8e6438
conan-sum <conansum24@gmail.com>
2021-12-31 18:32:34 -0500

    Changed the evaluate function to return a Model class with model used and the evaluation range

--- twilite/model_tuning/__init__.py
+++ twilite/model_tuning/__init__.py
@@ -2,26 +2,24 @@ from sklearn.metrics import silhouette_score
 
 
 class Model:
-    def __init__(self, model, eval_range):
-        self.model = model
+    def __init__(self, clustering, eval_range, parameter):
+        self.clustering = clustering
         self.eval_range = eval_range
-        self.parameter = None
+        self.parameter = parameter
 
 
 class Validate:
-    def __init__(self, algorithm, data):
-        self.algorithm = algorithm
-        self.data = data
+    def __init__(self, model, eval_range):
+        self.model = model
+        self.eval_range = eval_range
 
-    def evaluate(self):
-        df = self.data
+    def evaluate(self, df):
         S = []
         arr = df[['xcord', 'ycord']].to_numpy()
-        for i in self.algorithm.eval_range:
-            labels = self.algorithm.model(i).fit_predict(arr)
+        for i in self.eval_range:
+            labels = self.model(i).fit_predict(arr)
             score = silhouette_score(arr, labels, metric='euclidean')
             S.append((i, score))
         S.sort(key=lambda x: x[1], reverse=True)
-        self.algorithm.parameter = S[0][0]
-        return self.algorithm
-
+        model = Model(clustering=self.model, eval_range=self.eval_range, parameter=S[0][0])
+        return model

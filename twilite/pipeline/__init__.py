0eafe141cbde46a6e30499e5fca04282e59c9a64
Conan Sum <conansum24@gmail.com>
2022-02-20 20:48:44 -0500

    Added best parameter variable for #17

--- twilite/pipeline/__init__.py
+++ twilite/pipeline/__init__.py
@@ -1,13 +1,16 @@
 import seaborn as sns
+import numpy as np
 
 
 class Pipeline:
-    def __init__(self, preprocess, transform, evaluate):
+    def __init__(self, preprocess=None, transform=None, evaluate=None, database=None):
         self.preprocess = preprocess
         self.transform = transform
         self.evaluate = evaluate
-        self.model = None
-        self.results = None
+        self.database = database
+        self.best_param_ = None
+        self.labels = None
+        self.eval_report = None
 
     def fit(self, data):
         self.preprocess.read_df(data)
@@ -17,14 +20,25 @@ class Pipeline:
         self.transform.author_ids = _id
         self.transform.rescale()
         df = self.transform.projection()
-        self.model = self.evaluate.evaluate(df=df)
+        param = self.evaluate.eval(df=df)
+        self.best_param_ = param
         arr = df[['xcord', 'ycord']].to_numpy()
-        model = self.model.clustering(self.model.parameter)
+        model = self.evaluate.model(param)
         labels = model.fit_predict(arr)
         df['label'] = labels
-        self.results = df
+        self.labels = df
+        self.eval_report = np.array(self.evaluate.report).reshape(-1,2)
         return None
 
     def scatter_plot(self):
-        df = self.results
+        df = self.labels
         return sns.scatterplot(x=df['xcord'], y=df['ycord'], hue=['c ' + str(x) for x in df['label']])
+
+    def load_config(self, config_file=None, from_db=False, **kwargs):
+        pass
+
+    def save_config(self, config_file=None, to_db=False):
+        pass
+
+    def save_labels(self):
+        pass

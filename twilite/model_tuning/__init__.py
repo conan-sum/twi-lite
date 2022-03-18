a5f20d7bf46a7df2a356f253ac0e735ed87ea457
Conan Sum <conansum24@gmail.com>
2022-03-18 15:47:58 -0400

    Updated evaluation function for #14

--- twilite/model_tuning/__init__.py
+++ twilite/model_tuning/__init__.py
@@ -1,23 +1,17 @@
-class Model:
-    def __init__(self, clustering, eval_range, parameter):
-        self.clustering = clustering
-        self.eval_range = eval_range
-        self.parameter = parameter
-
-
 class Validation:
     def __init__(self, model, eval_range, metric):
         self.model = model
         self.eval_range = eval_range
         self.metric = metric
+        self.report = []
 
-    def evaluate(self, df):
+    def eval(self, df):
         S = []
         arr = df[['xcord', 'ycord']].to_numpy()
         for i in self.eval_range:
             labels = self.model(i).fit_predict(arr)
             score = self.metric(arr, labels)
             S.append((i, score))
+            self.report.append((i, score))
         S.sort(key=lambda x: x[1], reverse=True)
-        model = Model(clustering=self.model, eval_range=self.eval_range, parameter=S[0][0])
-        return model
+        return S[0][0]

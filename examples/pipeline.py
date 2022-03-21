7a5275986cfcb01e2ac721e955c852bd95b3c6eb
Conan Sum <conansum24@gmail.com>
2022-03-21 12:02:48 -0400

    Updated gitignore and example files

--- examples/pipeline.py
+++ examples/pipeline.py
@@ -3,23 +3,24 @@ import umap
 from twilite.preprocessing import Matrix
 from twilite.transformation import Decomposition
 from twilite.model_tuning import Validation
+from twilite.database import Storage
+from twilite.utils import connect
 from twilite.pipeline import Pipeline
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-df = pd.read_csv('data/data.csv')
+df = pd.read_csv('../data/data.csv')
 df.drop('Unnamed: 0', axis=1, inplace=True)
 
 hashtag = Pipeline(
+    feature='hashtag',
     preprocess=Matrix(filter_by='frequency', user_num=5, ft_freq=50, ft_num=3),
     transform=Decomposition(mapper=umap.UMAP(n_components=2)),
-    evaluate=Validation(model=KMeans, eval_range=range(2,10), metric=silhouette_score)
+    evaluate=Validation(model=KMeans, eval_range=range(2, 10), metric=silhouette_score),
+    database=Storage(connection=connect('alz'))
 )
 
 hashtag.fit(df)
-
-print(hashtag.results)
-print(hashtag.model.parameter)
 
 if __name__ == "__main__":
     pass

c42f65764d0aa0ed1549dae24b44a5fdfa6479e7
Conan Sum <conansum24@gmail.com>
2022-04-04 14:58:35 -0400

    Added progress report for pipeline

--- twilite/pipeline/__init__.py
+++ twilite/pipeline/__init__.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import numpy as np
 import time
 import warnings
+from joblib import logger
 warnings.filterwarnings('ignore')
 
 
@@ -22,12 +23,14 @@ class Pipeline:
             data = self.database.fetch(self.feature)
         self.preprocess.read_df(data)
         mat, _id = self.preprocess.sparse()
-        print(f'[ETL 1/4] COMPLETE .......... PREPROCESS, TOTAL TIME={time.time() - start}')
+        print(f'[ETL 1/4] COMPLETE .......... PREPROCESS, TOTAL TIME={logger.short_format_time(time.time() - start)}')
+
         split = time.time()
         self.transform.X = mat
         self.transform.author_ids = _id
         df = self.transform.projection()
-        print(f'[ETL 2/4] COMPLETE ...... TRANSFORMATION, TOTAL TIME={time.time() - split}')
+        print(f'[ETL 2/4] COMPLETE ...... TRANSFORMATION, TOTAL TIME={logger.short_format_time(time.time() - split)}')
+
         split = time.time()
         param = self.evaluate.eval(df=df)
         self.best_param_ = param
@@ -35,7 +38,8 @@ class Pipeline:
         model = self.evaluate.model(param)
         labels = model.fit_predict(arr)
         df['label'] = labels
-        print(f'[ETL 3/4] COMPLETE .... MODEL EVALUATION, TOTAL TIME={time.time() - split}')
+        print(f'[ETL 3/4] COMPLETE .... MODEL EVALUATION, TOTAL TIME={logger.short_format_time(time.time() - split)}')
+
         split = time.time()
         self.labels = df.sort_values(by='label')
         self.eval_report = np.array(self.evaluate.report).reshape(-1, 2)
@@ -43,8 +47,8 @@ class Pipeline:
             self.database.save_to_db(feature=self.feature, df=self.labels)
         else:
             self.labels.to_csv(f'{self.feature}_embeddings.csv')
-        print(f'[ETL 4/4] COMPLETE ........... LOAD DATA, TOTAL TIME={time.time() - split}')
-        print(f"[ETL 4/4] END ......... PROCESS COMPLETE, TOTAL TIME={time.time() - start}")
+        print(f'[ETL 4/4] COMPLETE ........... LOAD DATA, TOTAL TIME={logger.short_format_time(time.time() - split)}')
+        print(f'PROCESS COMPLETE ...................... , TOTAL TIME={logger.short_format_time(time.time() - start)}')
         return None
 
     def scatter_plot(self):
